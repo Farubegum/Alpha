c3ae2e31987e62e944161a684428595d0475fb87
Farubegum <42862209+Farubegum@users.noreply.github.com>
2019-02-07 16:11:49 +0530

    Create Alphabb.py

---
+++ Alphabb.py
@@ -0,0 +1,14 @@
+ca = input();
+
+if ca == '0':
+
+exit();
+
+else:
+
+    if((ca>='a' and ca<='z') or (ca>='A' and ca<='Z')):
+
+    	print(ca, "Alphabet");                                               else:
+    else:
+    	print(ca, "No");
+
